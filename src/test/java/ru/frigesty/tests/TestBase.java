3071232cb58f4edd638717b70792960427e9aea0
Airat Karimov <frigesty@gmail.com>
2025-03-13 15:13:28 +0300

    Rework code

--- src/test/java/ru/frigesty/tests/TestBase.java
+++ src/test/java/ru/frigesty/tests/TestBase.java
@@ -6,10 +6,13 @@ import io.qameta.allure.selenide.AllureSelenide;
 import io.restassured.RestAssured;
 import org.aeonbits.owner.ConfigFactory;
 import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import ru.frigesty.config.WebDriverConfig;
+import ru.frigesty.helpers.Attach;
+
 import java.util.Map;
 
 import static com.codeborne.selenide.Selenide.*;
@@ -52,15 +55,15 @@ public class TestBase {
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
     }
 
-  //  @AfterEach
- //   void addAttachments() {
-//        Attach.screenshotAs("Last screenshot");
-//        Attach.addVideo();
-  //      Attach.pageSource();
-  //      if (!Configuration.browser.equalsIgnoreCase("firefox")) {
-   //         Attach.browserConsoleLogs();
-  //      }
-  //  }
+    @AfterEach
+    void addAttachments() {
+        Attach.screenshotAs("Last screenshot");
+        Attach.addVideo();
+        Attach.pageSource();
+        if (!Configuration.browser.equalsIgnoreCase("firefox")) {
+            Attach.browserConsoleLogs();
+        }
+    }
 
     @AfterAll
     public static void tearDownWebDriver() {
